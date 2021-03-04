e36aca7c6313b6ae8003c113e0669ffb93792472
daltrowilliam <daltrowilliam@gmail.com>
2021-03-04 16:05:51 -0300

    Versão para entrega

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import express from "express";
 import { AddressInfo } from "net";
 import { userRouter } from "./controller/routes/userRouter";
 import { imageRouter } from "./controller/routes/imageRouter";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.json());
 
 app.use("/user", userRouter);
 app.use("/image", imageRouter);
+
 
 
 const server = app.listen(3003, () => {
